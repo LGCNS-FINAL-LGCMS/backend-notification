a94d061f10f57c0fb6628f1dbad8767ff6f7616b
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-08-28 10:55:34 +0900

    [LB-371] feat : redis 세팅

---
+++ src/main/kotlin/com/lgcms/notification/config/RedisConfig.kt
@@ -0,0 +1,46 @@
+package com.lgcms.notification.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.listener.RedisMessageListenerContainer
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig(
+    @Value("\${spring.data.redis.host}") private val redisHost: String,
+    @Value("\${spring.data.redis.port}") private val redisPort: Int,
+    @Value("\${spring.data.redis.database}") private val redisDatabase: Int,
+    private val objectMapper: ObjectMapper
+) {
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val connectionFactory = LettuceConnectionFactory(redisHost, redisPort)
+        connectionFactory.database = redisDatabase
+        return connectionFactory
+    }
+
+    @Bean
+    fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, String> {
+        val template = RedisTemplate<String, String>()
+        template.connectionFactory = connectionFactory
+        template.keySerializer = StringRedisSerializer()
+        template.valueSerializer = GenericJackson2JsonRedisSerializer(objectMapper)
+        return template
+    }
+
+    @Bean
+    fun redisMessageListenerContainer(
+        connectionFactory: RedisConnectionFactory
+    ): RedisMessageListenerContainer {
+        val container = RedisMessageListenerContainer()
+        container.setConnectionFactory(connectionFactory)
+        return container
+    }
+}
